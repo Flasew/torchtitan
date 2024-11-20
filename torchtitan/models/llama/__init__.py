6d547724d69efc18b9e40fe4fa988dc2d08dd950
Frank <weiyangw@mit.edu>
2024-11-20 19:26:30 +0000

    add gpt2

--- torchtitan/models/llama/__init__.py
+++ torchtitan/models/llama/__init__.py
@@ -15,10 +15,13 @@ llama2_configs = {
     "debugmodel": ModelArgs(dim=256, n_layers=8, n_heads=16, activation="GeLU"),
     "271M": ModelArgs(dim=1024, n_layers=16, n_heads=8),
     "1B": ModelArgs(dim=2048, n_layers=18, n_heads=16),
-    "1B_gpt3xl": ModelArgs(
+    "gpt3xl": ModelArgs(
         dim=2048, n_layers=24, n_heads=16, embed_type="position", activation="GeLU"
     ),
-    "7B_gpt": ModelArgs(
+    "gpt2": ModelArgs(
+        dim=1600, n_layers=48, n_heads=25, embed_type="position", activation="GeLU"
+    ),
+    "gpt3_6_7b": ModelArgs(
         dim=4096, n_layers=32, n_heads=32, embed_type="position", activation="GeLU"
     ),
     "7B": ModelArgs(dim=4096, n_layers=32, n_heads=32),
