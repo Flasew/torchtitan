871cf532f14694609588bdf5b10ccec1595de3a5
weiyangw <weiyangw@mit.edu>
2024-11-25 11:30:49 +0000

    add a few scripts

---
+++ run_pipeline_ib_baseline.sh
@@ -0,0 +1,15 @@
+#!/bin/bash 
+
+jobs=( llama2-7b llama2-13b llama3-8b )
+
+eval "$(conda shell.bash hook)"
+conda activate ckpt
+
+sleep 3
+
+source /data/frankwwy/innet_ckpt/scripts/set_ib_env.sh
+
+for job in ${jobs[@]}; do
+  /home/frankwwy/miniconda3/envs/ckpt/bin/python /home/frankwwy/miniconda3/envs/ckpt/bin/torchrun --nnodes=12 --nproc-per-node=1 --node-rank=$MY12RANK --rdzv-id=1 --rdzv-backend=c10d --rdzv-endpoint=uther train.py --job.config_file ./train_configs/${job}.toml --job.save_to_file $job-dpdk
+  sleep 30
+done
