02e76b2e030f780ff117aaca681929f48e40ff10
frank <weiyangw@mit.edu>
2024-11-27 16:26:31 +0000

    dpdk version of exp file

--- run_pipeline_exp.sh
+++ run_pipeline_exp.sh
@@ -8,9 +8,9 @@ conda activate ckpt
 
 sleep 3
 
-source /data/frankwwy/innet_ckpt/script/set_ib_env.sh
+source /data/frankwwy/innet_ckpt/script/set_dpdk_env.sh
 
 for job in ${jobs[@]}; do
-  /home/frankwwy/miniconda3/envs/ckpt/bin/python /home/frankwwy/miniconda3/envs/ckpt/bin/torchrun --nnodes=8 --nproc-per-node=1 --node-rank=$MY8RANK --rdzv-id=1 --rdzv-backend=c10d --rdzv-endpoint=midna train.py --job.config_file ./train_configs/${job}.toml --training.steps 20 # --job.save_to_file $job-dpdk
+   NCCL_BUFFSIZE=1048576 NCCL_P2P_NET_CHUNKSIZE=32768 sudo -E nice -n -20 numactl -m 0 -C 0-7,24-31 /home/frankwwy/miniconda3/envs/ckpt/bin/python /home/frankwwy/miniconda3/envs/ckpt/bin/torchrun --nnodes=8 --nproc-per-node=1 --node-rank=$MY8RANK --rdzv-id=1 --rdzv-backend=c10d --rdzv-endpoint=midna train.py --job.config_file ./train_configs/${job}.toml --training.steps 20 # --job.save_to_file $job-dpdk
   sleep 1
 done
